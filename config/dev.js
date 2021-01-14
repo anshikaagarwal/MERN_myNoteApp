0279a751adb6159b77e76b8233fc94916a4b7b78
AnshikaAgrawal <anshikaagrawal1089@gmail.com>
2021-01-14 16:30:41 +0530

    updated mongodb cluster for dev,prod and CI/CD

--- config/dev.js
+++ config/dev.js
@@ -2,24 +2,10 @@ module.exports = {
   googleClientID:
     '885893543058-hl90bq2pe7e7lo9tis315pbsg2mo47mq.apps.googleusercontent.com',
   googleClientSecret: 'j_QFV5XC1A5bCsUM3yVFMFMh',
-  mongoURI: 'mongodb+srv://anshika:anshika1089@mynoteapp3cluster-8nq8e.mongodb.net/test?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://anshika:anshika1089@cluster0.lmjxx.mongodb.net/<dbname>?retryWrites=true&w=majority',
   cookieKey: '123123123',
   redisurl: 'redis://127.0.0.1:6379'
 };
-
-
-// ma731 theory of numbers(1+1+1) t
-// ci511 networks(1+1+1) t
-// ci513 software(1+1+1) t
-// ci571 networks lab(1+)
-// ci573 software lab(1+)
-// ci576 information security lab done(1+)
-// ci575 open source lab(1+)
-
-// 19hs311 enterpreneur(1+1+) t
-
-
-// ci591 minor project done
 
 
 
